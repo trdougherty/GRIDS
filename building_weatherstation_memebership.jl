5b00980cf6b343fbec17e5051aef1e7d9e5c5527
Tom Dougherty <tomdougherty@DN525eov.SUNet>
2022-02-24 16:14:02 -0800

    adding notebook to process the station areas using a voronoi diagram

---
+++ building_weatherstation_memebership.jl
@@ -0,0 +1,38 @@
+### A Pluto.jl notebook ###
+# v0.18.0
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 9ca0e34a-95cf-11ec-1e52-8d4764b36bf2
+begin
+	using CSV
+	using GeoDataFrames
+	using DataFrames
+	using Plots
+end
+
+# ╔═╡ 780d4d98-1758-44d9-8114-120b5b0632ee
+
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.9.0-DEV.53"
+manifest_format = "2.0"
+project_hash = "da39a3ee5e6b4b0d3255bfef95601890afd80709"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╠═9ca0e34a-95cf-11ec-1e52-8d4764b36bf2
+# ╠═780d4d98-1758-44d9-8114-120b5b0632ee
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
