d8c7b178e8b8c106f88b2eb02abb0e83151505be
Thomas Dougherty <tomdou@stanford.edu>
2023-03-04 18:09:16 -0800

    updating to accomodate new data coming in

--- src/bridges/austin.py
+++ src/bridges/austin.py
@@ -3,6 +3,8 @@ import sys
 import shutil
 import json
 import yaml
+import glob
+from datetime import datetime
 
 from shapely import Point
 
@@ -10,6 +12,7 @@ from os import listdir
 from os.path import isfile, join
 
 import pandas as pd
+import numpy as np
 import geopandas as gpd
 import argparse
 from pathlib import Path
@@ -23,11 +26,9 @@ os.makedirs(args.output, exist_ok= True)
 
 auxillary = [ "footprints.geojson", "energy.csv" ]
 city_files = [ f for f in listdir(args.city) if isfile(join(args.city, f)) ]
-image_files = [ x for x in city_files if x not in auxillary and 'json' in x ]
+image_files = [ x for x in city_files if x not in auxillary and '.json' in x ]
 
-print(city_files)
 print(image_files)
-
 dflist = []
 
 imjson = None
@@ -43,11 +44,22 @@ for image_file in image_files:
 
     image_df['pano_date'] = [ '-'.join([ str(y) for y in x.values() ]) for x in image_df.pano_date ]
     cleaned_imagedf = image_df[["pano_id","pano_date","geometry","error"]]
-    cleaned_imagedf['filepath'] = [ os.path.join(os.path.splitext(image_src)[0], str(x))+'.png' for x in range(len(image_df)) ] #image_df['pano_id'] ]
+    cleaned_imagedf['filepath'] = [ os.path.join(os.path.splitext(image_src)[0], str(x))+'.png' for x in image_df['pano_id'] ]
     dflist.append(cleaned_imagedf)
 
 pano_metadata = pd.concat(dflist)
-print(pano_metadata)
+
+## check if the files exist before we transfer them and do the math with them
+print(f"Pano before: {pano_metadata}")
+pano_exists_list = [ os.path.exists(x) for x in pano_metadata['filepath'] ]
+pano_metadata = pano_metadata[pano_exists_list]
+print(f"Pano after: {pano_metadata}")
+pano_metadata = pano_metadata.loc[~pano_metadata['error']]
+pano_metadata = pano_metadata.reset_index().drop(columns=['index'])
+
+# now convert it all to hash and drop the collisions
+pano_metadata['pano_id'] = [ str(hash(x) % ((sys.maxsize + 1) * 2)) for x in pano_metadata['pano_id'] ]
+pano_metadata = pano_metadata.drop_duplicates(subset=["pano_id"], keep=False).reset_index().drop(columns='index')
 
 config = None
 config_path = os.path.join(args.city, "config.yml")
@@ -64,41 +76,38 @@ footenergy = footprints.merge(energy, left_on="id", right_on="footprint_id", how
 footenergy['geometry'] = footenergy.geometry.buffer(config['cleaningradius'])
 
 combined = gpd.sjoin(footenergy, pano_metadata.to_crs(config['projection']), predicate="contains")
-combined = combined.loc[~combined['error']]
+
+
+
+# print(combined)
+# print(pano_metadata)
 
 keeping_panos = pano_metadata.iloc[combined['index_right'].unique()]
+
+# want to try and make sure we only have one copy of each geometry from the panos
+keeping_panos['geomhash'] = [ str(hash(x)) for x in keeping_panos.geometry ]
+keeping_panos = keeping_panos.drop_duplicates(subset=['geomhash'], keep=False)
 
 keepingpanoids = combined.id.unique()
 energy_c = energy.loc[energy['footprint_id'].isin(keepingpanoids)]
 energy_c.to_csv(os.path.join(args.output, "energy.csv"), index=False)
 
 footprint_c = footprints.loc[footprints['id'].isin(keepingpanoids)]
-footprint_c.to_crs(4326).to_file(os.path.join(args.output, "footprints.gejson"), driver="GeoJSON")
+footprint_c.to_crs(4326).to_file(os.path.join(args.output, "footprints.geojson"), driver="GeoJSON")
 
-print(keeping_panos)
+# print(keeping_panos)
 
-keeping_panos['pano_id'] = [ str(hash(x) % ((sys.maxsize + 1) * 2)) for x in keeping_panos['pano_id'] ]
+# keeping_panos['pano_id'] = [ str(hash(x) % ((sys.maxsize + 1) * 2)) for x in keeping_panos['pano_id'] ]
+# keeping_panos = keeping_panos.drop_duplicates(subset=["pano_id"], keep='first').reset_index().drop(columns='index')
 
 outimagedir = os.path.join(args.output, "images")
 os.makedirs(outimagedir, exist_ok=True)
+files = glob.glob(outimagedir+"/*")
+for f in files:
+    os.remove(f)
+
 for index, row in keeping_panos.iterrows():
-    newfilename = os.path.join(outimagedir, str(row['pano_id']) + '.png')
+    newfilename = os.path.join(outimagedir, str(row['pano_id']) + os.path.splitext(row['filepath'])[1])
     shutil.copyfile(row['filepath'], newfilename)
 
 keeping_panos[["pano_id","pano_date","geometry"]].to_file(os.path.join(args.output, "nodes.geojson"), driver="GeoJSON")
-
-# print(combined)
-
-# print(footprints)
-# print(footenergy)
-# print(pano_metadata.to_crs(config['projection']))
-
-# print(pano_metadata)
-    # print(cleaned_imagedf)
-
-    # dflist.append(image_df[["pano_id","pano_date","geometry"]][~image_df["error"]])
-    
-    # imagedirname = os.path.splitext(image_file)[0]
-    # imagedirpath = os.path.join(args.city, imagedirname)
-
-    # for pid in 
